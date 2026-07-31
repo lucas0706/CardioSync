c22745674d856e9365217a2ed241b05ff994c57a
Lucas <TU_EMAIL_DE_GITHUB>
2026-07-31 23:13:02 +0000

    feat: CardioSync clinical guide

--- src/clinical/index.ts
+++ src/clinical/index.ts
@@ -1,5 +1,11 @@
 export * from './engine'
+
 export * from './guides'
+
 export * from './types/ClinicalGuide'
 
+export * from './shared/ClinicalClassification'
+
 export * from './esc2024/classification'
+
+export * from './cardiosync/classification'
